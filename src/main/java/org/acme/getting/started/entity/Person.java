b25c1a7c49b309029c522f1461d3eb70dd72d2c1
NetoDevel <josevieira.dev@gmail.com>
2021-04-25 20:45:09 -0300

    Adds liquibase

--- src/main/java/org/acme/getting/started/entity/Person.java
+++ src/main/java/org/acme/getting/started/entity/Person.java
@@ -3,10 +3,12 @@ package org.acme.getting.started.entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "person")
 public class Person extends PanacheEntity {
 
     @NotBlank(message = "name my not be blank")
